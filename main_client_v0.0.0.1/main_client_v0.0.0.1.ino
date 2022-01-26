892440829b5c1960c9db2ea718d889b0f2c7df85
DerMaxl1112 <maximilian-hug@gmx.de>
2022-01-26 19:57:05 +0100

    Es Funktioniert

--- main_client_v0.0.0.1/main_client_v0.0.0.1.ino
+++ main_client_v0.0.0.1/main_client_v0.0.0.1.ino
@@ -1,8 +1,15 @@
 
-int column_1 = 2, column_2 = 3, column_3 = 4, column_4 = 5; // Spalten der Tasten
+int column_1 = 0, column_2 = 0, column_3 = 0, column_4 = 0;
+int column_1_pin = 2, column_2_pin = 3, column_3_pin = 4, column_4_pin = 5; // Spalten der Tasten
 int row_1 = 12, row_2 = 11, row_3 = 10; // Zeilen der Tasten
 
-int pot_out_1 = 6, pot_out_2 = 7, pot_out_3 = 8, pot_out_4 = 9 // Poti Stromversorgung
+int pot_out_1 = 6, pot_out_2 = 7, pot_out_3 = 8, pot_out_4 = 9; // Poti Stromversorgung
+
+
+bool switches[12];
+
+int pot[6];
+int pot_alt[6];
 
 
 void setup() {
@@ -11,10 +18,10 @@ void setup() {
   Serial.begin(9600); // initialize serial communication at 9600 bits per second
 
   // pins zu input und outputs setzen
-  pinMode(column_1, INPUT);
-  pinMode(column_2, INPUT);
-  pinMode(column_3, INPUT);
-  pinMode(column_4, INPUT);
+  pinMode(column_1_pin, INPUT);
+  pinMode(column_2_pin, INPUT);
+  pinMode(column_3_pin, INPUT);
+  pinMode(column_4_pin, INPUT);
 
   pinMode(row_1, OUTPUT);
   pinMode(row_2, OUTPUT);
@@ -32,18 +39,11 @@ void setup() {
   digitalWrite(pot_out_3, HIGH);
   digitalWrite(pot_out_4, HIGH);
   
-  
-
-  bool switches[12];
-  
   for(int i = 0; i < 12; i++)
     switches[i] = false;
-  
-  
-  int pot[6];
-  int pot_alt[6];
-  
-  for(int i = 0; i < 12; i++)
+
+
+  for(int i = 0; i < 6; i++)
     pot[i] = 0;
   
 }
@@ -54,6 +54,11 @@ void loop() {
   
   // Tasten auslesen
   digitalWrite(row_1, HIGH);
+
+  column_1 = digitalRead(column_1_pin);
+  column_2 = digitalRead(column_2_pin);
+  column_3 = digitalRead(column_3_pin);
+  column_4 = digitalRead(column_4_pin);
 
   if(column_1 == HIGH)
     switches[1] = true;
@@ -79,6 +84,11 @@ void loop() {
   
   digitalWrite(row_2, HIGH);
 
+  column_1 = digitalRead(column_1_pin);
+  column_2 = digitalRead(column_2_pin);
+  column_3 = digitalRead(column_3_pin);
+  column_4 = digitalRead(column_4_pin);
+
   if(column_1 == HIGH)
     switches[5] = true;
   else
@@ -87,7 +97,7 @@ void loop() {
   if(column_2 == HIGH)
     switches[6] = true;
   else
-    switches[6] = false
+    switches[6] = false;
 
   if(column_3 == HIGH)
     switches[7] = true;
@@ -103,6 +113,11 @@ void loop() {
   
   digitalWrite(row_3, HIGH);
 
+  column_1 = digitalRead(column_1_pin);
+  column_2 = digitalRead(column_2_pin);
+  column_3 = digitalRead(column_3_pin);
+  column_4 = digitalRead(column_4_pin);
+
   if(column_1 == HIGH)
     switches[9] = true;
   else
@@ -111,7 +126,7 @@ void loop() {
   if(column_2 == HIGH)
     switches[10] = true;
   else
-    switches[10] = false
+    switches[10] = false;
 
   if(column_3 == HIGH)
     switches[11] = true;
@@ -144,8 +159,17 @@ void loop() {
   
   // übertragen an pc: pot
   // übertragen an pc: switches
+
+  //for(int i = 0; i < 12; i++){
+    //Serial.println(switches[i]);
+  //}
+
   
-  serial.println(switches)
-  serial.println(pot)
+  //for(int i = 0; i < 6; i++){
+    //Serial.println(pot[i]);
+  //}
+
+  Serial.println(pot[0]);
+  
   
 }
