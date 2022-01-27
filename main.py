21efcc2ee57607510a78c5cc774ad7e4de423080
Maximilian Hug <maximilian-hug@gmx.de>
2022-01-27 09:19:00 +0100

    Fix 9600

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import serial
 #info auf https://maker.pro/pic/tutorial/introduction-to-python-serial-ports
 
 
-serialPort = serial.Serial(port = "COM4", baudrate=9800, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort = serial.Serial(port = "COM4", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
